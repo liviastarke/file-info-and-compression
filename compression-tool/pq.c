c740625ad7b94e76b21654387ab453c0d5f06a4c
vitoria bianca camargo <agk2084>
2026-09-20 21:14:15 -0300

    ja nem sei o que fazer

--- compression-tool/pq.c
+++ compression-tool/pq.c
@@ -1,37 +1,37 @@
 /**
  * @file pq.c
- * @brief implementação do min-heap para nós huffman (HuffmanNode)
+ * @brief Implementação do Min-Heap para nós Huffman (HuffmanNode)
  */
 
-#include "pq.h" //min-heap guarda huffmanNode
+#include "pq.h"
 
 /**
- * @brief insere um nó na fila de prioridade (min-heap)
+ * @brief Insere um nó na fila de prioridade (Min-Heap)
  */
-void pqPush(MinHeap *pq, HuffmanNode *nd) {
+void pq_push(MinHeap *pq, HuffmanNode *nd)
+{
     pq->nodes[pq->size] = nd;
     pq->size++;
 
-    //guarda a posição do novo 'nó'
-    //declaracao de i. representa o índice do nó atual ???
+    // Guarda a posição do novo nó
     int i = pq->size - 1;
 
-    while (pq->size > 1) {
-        // calcula o índice do nó pai
-        int parent = (i > 0) / 2;
+    while (i > 0) {
 
-        // caso nó pai tiver frequência menor ou igual à frequência do nó atual
-        if (pq->nodes[parent]->freq <= pq->nodes[pq->size - 1]->freq) {
+        // Calcula o índice do nó pai
+        int parent = (i - 1) / 2;
+
+        // Se o pai tiver frequência menor ou igual,
+        if (pq->nodes[parent]->freq <= pq->nodes[i]->freq) {
             break;
         }
 
-        // troca o nó atual com o nó pai
+        // Troca o nó atual com o nó pai
         HuffmanNode *temp = pq->nodes[parent];
-        pq->nodes[parent] = pq->nodes[pq->size - 1];
-        //pq->nodes[pq->size - 1] = temp;
+        pq->nodes[parent] = pq->nodes[i];
         pq->nodes[i] = temp;
-        
-        //nó atualiza, agora o nó pai é o nó atual
+
+        // o nó agora está na posição do antigo pai
         i = parent;
     }
 }
--- compression-tool/pq.h
+++ compression-tool/pq.h
@@ -20,8 +20,31 @@ typedef struct {
 /**
  * @brief inicializa a fila de prioridade (min-heap)
  */
-void pq_push(MinHeap *pq, HuffmanNode *node);
-HuffmanNode *pq_pop(MinHeap *pq);
+void pq_push(MinHeap *pq, HuffmanNode *nd) {
+
+    //colocar o nó na última posição
+
+    //inserir o nó na última posição do heap
+
+    //fazer nó subir até a posição correta enquanto a frequência do nó pai for maior que a frequência do nó atual
+    /*
+    while() {
+        //sei la
+    }
+    */
+    //trocar o nó atual com o nó pai
+
+    //continuar a partir da posição do nó pai
+    
+}
+
+HuffmanNode *pq_pop(MinHeap *pq) {
+
+    if (pq->size == 0) {
+        //printf("Fila vazia.\n");
+        return NULL;
+    }    
+}
 
 //PQ_H
 #endif
